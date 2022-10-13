323e9e2306870a44e169a64cf6e5a091a55de016
cooperemily02 <ecooper@umass.edu>
2022-10-13 13:44:25 -0400

    import MUI

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import Button from '@mui/material/Button'
 import './App.css';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Button variant="outlined">Press Me</Button>
         <a
           className="App-link"
           href="https://reactjs.org"
