1b63d04b61c2864318b5b04bed353e84ecd70ee0
egpickard <92379477+egpickard@users.noreply.github.com>
2022-10-14 14:46:43 -0400

    Update api.py

--- backend/api.py
+++ backend/api.py
@@ -5,41 +5,45 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
-  
-@app.route('/Dailyoutfits', methods = ['GET'])
+
+@app.route('/dummy/Dailyoutfits', methods = ['GET'])
 def Return_Outfits():
-
     if(request.method == 'GET'):
-        data = {
-            "Outfit I" : ("Black rain coat", "White t-shirt", "black jeans", "rainboots"), 
-            "Outfit II" : ("Black rain coat", "black long sleeve", "black jeans", "rainboots"), 
-            "Outfit III" : ("Black rain coat", "White t-shirt" , "high waisted blue jeans", "rainboots"), 
-            "Outfit IV" : ("Black rain coat", "black long sleeve" , "high waisted blue jeans", "rainboots")
+        #"Outfit III" : ("Black rain coat", "White t-shirt" , "high waisted blue jeans", "rainboots"),
+        data = {"outfits":
+        [[{'name': 'black jeans', 'tag':'bottom'},{'name': 'black rain coat', 'tag':'outerwear'},{'name': 'shoes', 'tag':'rain boots'},{'name': 'white t-shirt', 'tags':'top'}],
+        [{'name': 'black jeans', 'tag':'bottom'},{'name': 'black rain coat', 'tag':'outerwear'},{'name': 'shoes', 'tag':'rain boots'},{'name': 'black long sleeve', 'tags':'top'}],
+        [{'name': 'high waisted blue jeans', 'tag':'bottom'},{'name': 'black rain coat', 'tag':'outerwear'},{'name': 'shoes', 'tag':'rain boots'},{'name': 'black long sl', 'tags':'top'}]]
+        ]
         }
-
         return jsonify(data)
     
 
-@app.route('/Closet', methods = ['GET'])
+@app.route('/dummy/Closet', methods = ['GET'])
 def Return_Closet():
     if(request.method == 'GET'):
-        data = {
-            "outerwear": ("Black rain coat"),
-            "tops" : ("White t-shirt", "black long sleeve"), 
-            "bottoms": ("black jeans", "high waisted blue jeans"), 
-            "shoes": ("rainboots")
-        }
+        data = [
+
+            {'name': "Black rain coat", 'tag': "outerwear"}, 
+            {'name': "White t-shirt", 'tag': "top"}, 
+            {'name': "black long sleeve", 'tag': "top"}, 
+            {'name': "black jeans", 'tag': "bottom"}, 
+            {'name': "high waisted blue jeans", 'tag': "bottom"}, 
+            {'name': "rainboots", 'tag': "shoe"}
+        ]
   
         return jsonify(data)
 
-@app.route('/Laundry', methods = ['GET'])
+@app.route('/dummy/Laundry', methods = ['GET'])
 def Return_Laundry():
     if(request.method == 'GET'):
-        data = {
-            "outerwear": ("blue rain coat"), 
-            "tops" : ("gray t-shirt", "white long sleeve"), 
-            "bottoms": ("brown jeans", "pink shorts")
-        }
+        data = [
+            {'name': "blue rain coat", 'tag': "outerwear"}, 
+            {'name': "gray t-shirt", 'tag': "top"}, 
+            {'name': "white long sleeve", 'tag': "top"}, 
+            {'name': "brown jeans", 'tag': "bottom"}, 
+            {'name': "pink shorts", 'tag': "bottom"}
+        ]
   
         return jsonify(data)
 
