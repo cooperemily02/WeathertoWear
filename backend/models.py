fb2c0853bd16f5b27de38db75c2f1461994e9f61
Saif Masoud <saifemasoud@gmail.com>
2022-10-19 21:28:10 -0400

    Renamed associative table to item_tags, it was misleading before

--- backend/models.py
+++ backend/models.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy(app)
 
 
-tags = db.Table('item_tags',
+item_tags = db.Table('item_tags',
     db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'), primary_key=True),
     db.Column('item_id', db.Integer, db.ForeignKey('clothing_item.id'), primary_key=True)
 )
