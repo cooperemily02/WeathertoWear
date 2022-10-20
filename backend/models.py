b95849bc1b88d6f7d052616f9eae13bb707929e5
Saif Masoud <saifemasoud@gmail.com>
2022-10-19 21:32:11 -0400

    Comment clarifiying item_tags table

--- backend/models.py
+++ backend/models.py
@@ -4,6 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy(app)
 
 
+# This table relates items & tags to each other (many-to-many relationship)
+# Also notice how SQLAlchemy maps the 'ClothingItem' class to 'clothing_item'
 item_tags = db.Table('item_tags',
     db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'), primary_key=True),
     db.Column('item_id', db.Integer, db.ForeignKey('clothing_item.id'), primary_key=True)
