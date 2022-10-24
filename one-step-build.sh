71cfdc2467a7e1c7f971c26f53f5b689c08966d8
Saif Masoud <saifemasoud@gmail.com>
2022-10-24 18:24:25 -0400

    Build script: Use 'python' or 'python3'

--- one-step-build.sh
+++ one-step-build.sh
@@ -4,7 +4,7 @@ npm run build
 mv ./build ../backend
 cd ..
 cd  backend
-python -m venv venv
+python -m venv venv || python3 -m venv venv
 . venv/Scripts/activate || . venv/bin/activate
 pip install -r requirements.txt
 python init_db.py
