bafd6bf92d5c192bfb92270e21b2674aa3e77d57
cooperemily02 <ecooper@umass.edu>
2022-11-03 19:26:01 -0400

    change to db to reflect different item types

--- backend/init_db.py
+++ backend/init_db.py
@@ -12,17 +12,31 @@ with app.app_context(): # context is needed so sqlalchemy knows where to create 
     item1 = models.ClothingItem(name="t-shirt")
     item2 = models.ClothingItem(name="jeans")
     item3 = models.ClothingItem(name="boots")
+    item4 = models.ClothingItem(name= "jacket")
 
     tag1 = models.Tag(name='cotton')
     tag2 = models.Tag(name='dark wash')
     tag3 = models.Tag(name='rain-proof')
 
-    item1.tags.append(tag1)
-    item2.tags.append(tag2)
-    item3.tags.append(tag3)
+    tag4 = models.Tag(name='top')
+    tag5 = models.Tag(name='bottom')
+    tag6 = models.Tag(name='shoes')
+    tag7 = models.Tag(name='outerwear')
 
-    db.session.add_all([item1, item2, item3]) # won't be written until the 'commit' line
-    db.session.add_all([tag1, tag2, tag3])
+
+
+
+    item1.tags.append(tag1)
+    item1.tags.append(tag4)
+    item2.tags.append(tag2)
+    item2.tags.append(tag5)
+    item3.tags.append(tag3)
+    item3.tags.append(tag6)
+    item4.tags.append(tag7)
+
+
+    db.session.add_all([item1, item2, item3, item4]) # won't be written until the 'commit' line
+    db.session.add_all([tag1, tag2, tag3, tag4, tag5, tag6, tag7])
     db.session.add_all([user1, user2])
     db.session.commit() # This writes the items to the database
 
