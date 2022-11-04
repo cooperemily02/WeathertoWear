ee6432016c4674836d30f8657be8fe0b48e8e04f
cooperemily02 <ecooper@umass.edu>
2022-11-03 21:41:16 -0400

    init commit

--- my-app/src/component/navbar.js
+++ my-app/src/component/navbar.js
@@ -6,7 +6,11 @@ import {
   Typography,
   useTheme,
   useMediaQuery,
+  MenuItem,
+  Menu,
 } from "@mui/material";
+import {Link} from "react-router-dom";
+import AccountCircleRoundedIcon from '@mui/icons-material/AccountCircleRounded';
 import { makeStyles } from "@mui/styles";
 import Drawer from "./Drawer";
 import IconButton from '@mui/material/IconButton';
@@ -39,11 +43,22 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+
+
 function Navbar(props) {
   const classes = useStyles();
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down("md"));
   const userId = props.userId
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    props.setUserId(-1)
+    setAnchorEl(null)
+  };
   return (
     <AppBar position="static" sx={{backgroundColor: 'rgb(116, 141, 166)'}}>
       <CssBaseline />
@@ -64,8 +79,37 @@ function Navbar(props) {
             Weather to Wear
           </Typography>
         </div>
+        <div style = {{
+            position: "absolute",
+            left: "95%",
+            top: "50%",
+            transform: "translate(-50%, -50%)",}}>
+          { userId !== -1 && 
+          <IconButton onClick={handleClick}>
+            <AccountCircleRoundedIcon style = {{color: 'white'}} fontSize = "large" />
+          </IconButton>
+          }
+          <Menu
+            id="demo-positioned-menu"
+            aria-labelledby="demo-positioned-button"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            anchorOrigin={{
+              vertical: 'top',
+              horizontal: 'left',
+            }}
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'left',
+            }}
+          >
+            <MenuItem component={Link} to = '/' onClick={handleClose} >Logout</MenuItem>
+        </Menu>
+      </div>
       </Toolbar>
     </AppBar>
+
   );
 }
 export default Navbar;
