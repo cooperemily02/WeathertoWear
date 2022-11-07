89269eb9fe6ef7d8b8c7f12adb0dac09c5a26041
Saif Masoud <saifemasoud@gmail.com>
2022-11-07 14:11:30 -0500

    Initial outfit generation logic code

---
+++ backend/w2w_logic/outfit_generator.py
@@ -0,0 +1,46 @@
+from typing import Set
+
+
+DEFAULT_OUTFIT_TEMPLATE = [
+    set(['top']),
+    set(['bottom']),
+    set(['shoes'])
+]
+DEFAULT_OUTFIT_TEMPLATE_SNOWY = [
+    set(['top', 'snowy']),
+    set(['bottom', 'snowy']),
+    set(['shoes', 'snowy'])
+]
+DEFAULT_OUTFIT_TEMPLATE_RAINY = [
+    set(['top', 'rainy']),
+    set(['bottom', 'rainy']),
+    set(['shoes', 'rainy'])
+]
+
+
+# Right now, weather_str is supposed matches the weather.py file's output
+def get_template_from_weather_str(weather_str):
+    if ('snowy' in weather_str):
+        return DEFAULT_OUTFIT_TEMPLATE_SNOWY
+    if ('rainy' in weather_str):
+        return DEFAULT_OUTFIT_TEMPLATE_RAINY
+    return DEFAULT_OUTFIT_TEMPLATE
+
+def get_matching_outfit(outfit_template, items: Set['Item']):
+    outfit = []
+    for attr_set in outfit_template:
+        for item in items:
+            if all(required_attr in item.attributes for required_attr in attr_set):
+                outfit.append(item)
+                items.remove(item) # Item has been added to outfit, so remove it from items
+                break # We are done with matching an item for the current attr_set
+            raise ValueError(f'Failed to find item matching: {attr_set}')
+    return outfit
+                
+
+
+class Item:
+    def __init__(self, name, attributes):
+        self.name = name
+        self.attributes = attributes
+
