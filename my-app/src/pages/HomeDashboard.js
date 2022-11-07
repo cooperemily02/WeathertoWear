145332dd6b19bbe16c25e9b00cf9c7e798b326b1
sp2418 <sapna.pariha@gmail.com>
2022-11-07 16:17:49 -0500

    Added weather dashboard to home page

--- my-app/src/pages/HomeDashboard.js
+++ my-app/src/pages/HomeDashboard.js
@@ -1,16 +1,17 @@
 import React, {useState} from "react";
 import Outfit from "../component/Outfit";
 import { Typography } from "@mui/material";
+import WeatherDashboard from "../component/WeatherDashboard";
 
 
 export const HomeDashboard = (props) => {
-    let zipcode = 1;
     let [outfit, setOutfit] = useState({top: 'Nike tank top', bottom: 'blue lulu shorts', shoes: 'adidas sneakers', outerwear: 'denim jacket'});
     console.log(outfit);
     return (
         <>
-            <Typography variant="h2" textAlign={'center'} sx={{color: 'white', fontFamily: 'Caudex', py: 15, mb:10, backgroundColor:'rgb(191, 172, 224)'}} >Hi, User {props.userId}</Typography>  
-            <Outfit zipcode={zipcode} />
+            <Typography variant="h2" textAlign={'center'} sx={{color: 'white', fontFamily: 'Caudex', py: 15, mb:10, backgroundColor:'rgb(191, 172, 224)'}} >Hi, User {props.userId}</Typography>
+            <WeatherDashboard />  
+            <Outfit />
         </>
     );
 };
