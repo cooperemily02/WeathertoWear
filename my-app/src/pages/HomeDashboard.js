19867e659143098e9636688ee3ff359273973424
cooperemily02 <ecooper@umass.edu>
2022-11-07 18:10:49 -0500

    add failure case

--- my-app/src/pages/HomeDashboard.js
+++ my-app/src/pages/HomeDashboard.js
@@ -6,6 +6,7 @@ import WeatherDashboard from '../component/WeatherDashboard'
 
 export const HomeDashboard = (props) => {
     const [zipcode, setZipcode] = useState(-1)
+    const [fetchedOutfitData, setFetchedOutfitData] = useState({hasOutfit: false, fetchError: false})
     const userId = props.userId
     let [outfit, setOutfit] = useState([]);
 
@@ -25,8 +26,12 @@ export const HomeDashboard = (props) => {
                },
             });
             const json = await response.json();
+            console.log("here and not an error. the returned json looks like:")
+            console.log(json)
             setOutfit(json)
+            setFetchedOutfitData({hasOutfit: true})
           } catch (error) {
+            setFetchedOutfitData({hasOutfit: false, fetchError: true})
             console.log("error", error);
           }
         };
@@ -34,9 +39,12 @@ export const HomeDashboard = (props) => {
         <>
             <Typography variant="h2" textAlign={'center'} sx={{color: 'white', fontFamily: 'Caudex', py: 15, mb:10, backgroundColor:'rgb(191, 172, 224)'}} >Hi, User {props.userId}</Typography>
             <div>
-            <WeatherDashboard zipCode = {zipcode} setZipcode = {setZipcode} fetchGeneratedOutfit = {fetchGeneratedOutfit}/>
-            {zipcode !== -1 && 
+            <WeatherDashboard zipCode = {zipcode} setZipcode = {setZipcode} fetchGeneratedOutfit = {fetchGeneratedOutfit} setHasOutfit = {setFetchedOutfitData}/>
+            {zipcode !== -1 && fetchedOutfitData.hasOutfit == true &&
             <Outfit zipcode={parseInt(zipcode)} outfit = {outfit}/>
+            }
+            {fetchedOutfitData.hasOutfit == false && fetchedOutfitData.fetchError == true && 
+                <h3> Unable to fetch an outfit at the current time. Please try again later or submit a trouble ticket. </h3>
             }
             </div> 
         </>
