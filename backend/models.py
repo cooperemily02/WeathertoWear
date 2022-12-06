f6add2ca0214d8fd372b26653fa9040ee15dc852
Saif Masoud <saifemasoud@gmail.com>
2022-12-05 22:55:09 -0500

    Fix item_tags foreign column reference

--- backend/models.py
+++ backend/models.py
@@ -7,7 +7,7 @@ db = SQLAlchemy()
 # Also notice how SQLAlchemy maps the 'ClothingItem' class to 'clothing_item'
 item_tags = db.Table(
     "item_tags",
-    db.Column("tag_name", db.String(50), nullable=False, primary_key=True),
+    db.Column("tag_name", db.String(50), db.ForeignKey('tag.name'), nullable=False, primary_key=True),
     db.Column(
         "item_id", db.Integer, db.ForeignKey("clothing_item.id"), primary_key=True
     ),
