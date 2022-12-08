c1e2068525f9e523015182f0148a1f636c534958
Saif Masoud <saifemasoud@gmail.com>
2022-12-08 15:36:31 -0500

    Revert "general housekeeping"
    This reverts commit e6788e60eae71b9b6dbec947451d965a7742bb50.

--- my-app/src/component/navbar.js
+++ my-app/src/component/navbar.js
@@ -55,11 +55,11 @@ function Navbar(props) {
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
   };
-  const logoutAndHandleClose = () => {
-    props.setUser({userName: "", userId: 0})
+  const handleClose = () => {
+    props.setUser({})
     setAnchorEl(null)
   };
-  const handleCloseMenu = () => {
+  const logoutHandleClose = () => {
     setAnchorEl(null)
   }
   return (
@@ -84,7 +84,7 @@ function Navbar(props) {
             left: "95%",
             top: "50%",
             transform: "translate(-50%, -50%)",}}>
-          { userId !== 0 && 
+          { userId !== -1 && 
           <IconButton onClick={handleClick}>
             <AccountCircleRoundedIcon style = {{color: 'white'}} fontSize = "large" />
           </IconButton>
@@ -94,7 +94,7 @@ function Navbar(props) {
             aria-labelledby="demo-positioned-button"
             anchorEl={anchorEl}
             open={open}
-            onClose={handleCloseMenu}
+            onClose={logoutHandleClose}
             anchorOrigin={{
               vertical: 'top',
               horizontal: 'left',
@@ -104,8 +104,7 @@ function Navbar(props) {
               horizontal: 'left',
             }}
           >
-            <MenuItem component={Link} to = '/' onClick={logoutAndHandleClose} >Logout</MenuItem>
-            <MenuItem component = {Link} to = '/outfit-template-form' onClick = {handleCloseMenu}>Outfit Template Form</MenuItem>
+            <MenuItem component={Link} to = '/' onClick={handleClose} >Logout</MenuItem>
         </Menu>
       </div>
       </Toolbar>
