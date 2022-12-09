aef92255f63363c05a5492252dc7c1270b0bfaac
Saif Masoud <saifemasoud@gmail.com>
2022-12-09 18:23:39 -0500

    Fix template picker not changing outfit type

--- backend/models.py
+++ backend/models.py
@@ -179,7 +179,7 @@ class OutfitTemplate(db.Model):
 
     @property
     def serialize(self):
-        return {'name': self.name}
+        return {'name': self.name, 'id': self.id}
 
 
 template_tags = db.Table(
