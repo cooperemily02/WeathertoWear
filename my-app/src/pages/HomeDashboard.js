8fa7d9e66032a874fd6d84dafa7291edc9d4e82b
Saif Masoud <saifemasoud@gmail.com>
2022-12-09 17:52:53 -0500

    Return outfitTemplate picker, manually merged 'HomeDashboard'

--- my-app/src/pages/HomeDashboard.js
+++ my-app/src/pages/HomeDashboard.js
@@ -3,32 +3,32 @@ import Outfit from "../component/Outfit";
 import { Typography, TextField, Button } from "@mui/material";
 import WeatherDashboard from '../component/WeatherDashboard'
 import postData from "../utils";
+import { LocalConvenienceStoreOutlined } from "@mui/icons-material";
 
 
 export const HomeDashboard = (props) => {
-    function isValidUSZip(sZip) {
-      return /^\d{5}(-\d{4})?$/.test(sZip);
-    }
     const [zipcode, setZipcode] = useState(-1)
     const [enteredZipcode, setEnteredZipcode] = useState("")
     const [fetchedOutfitData, setFetchedOutfitData] = useState({hasOutfit: false, fetchError: false})
     const [weather, setWeather] = useState(false)
-    const user = props.user
-    const userId = props.user.userId
-    //console.log(user);
+    const userId = props.userId
     let [outfit, setOutfit] = useState([]);
+    const [templateOptions, setTemplateOptions] = useState([])
+    const [templateId, setTemplateId] = useState(-1)
 
     //console.log(outfit);
     //console.log(zipcode);
     const fetchGeneratedOutfit = async () => {
+      const body = JSON.stringify({
+        user: userId,
+        zipcode: enteredZipcode,
+        template_id: templateId
+      })
         try {
             const response = await fetch("/gen-outfit", {
               method: "POST",
               credentials: "include",
-              body: JSON.stringify({
-                user: user.userId,
-                zipcode: enteredZipcode
-               }),
+              body: body,
                headers: {
                  'Content-type': 'application/json; charset=UTF-8',
                },
@@ -38,9 +38,7 @@ export const HomeDashboard = (props) => {
             setFetchedOutfitData({hasOutfit: true})
           } catch (error) {
             setFetchedOutfitData({hasOutfit: false, fetchError: true})
-            //handle error message - not working at the moment
-            console.log(error)
-            
+            console.log("error", error);
           }
         };
       const handleSetZipcode = (zipCode) => {
@@ -48,14 +46,11 @@ export const HomeDashboard = (props) => {
       }
 
       const handleButtonClick = () => {
-        if(isValidUSZip(enteredZipcode)){
-          setZipcode(enteredZipcode)
-          fetchGeneratedOutfit()
-          fetchWeatherData()
-        }
-        else{
-          alert("Entered ZipCode is invalid. Please try again.");
-        }
+        setZipcode(enteredZipcode)
+        setTemplateId(-1)
+        updateTemplateOptions()
+        fetchGeneratedOutfit()
+        fetchWeatherData()
       }
       const fetchWeatherData = async () => {
         try {
@@ -76,11 +71,23 @@ export const HomeDashboard = (props) => {
         setOutfit(_outfit)
       }
 
+      const onTemplateChange = (option) => {
+        setTemplateId(option.value)
+      }
+
+      const updateTemplateOptions = async () => {
+        const newTemplates = await postData('outfit-templates', {'user': 1})
+        const newTemplateOptions = []
+        newTemplates.forEach(element => {
+          newTemplateOptions.push({label: element.name, value: element.id})
+        });
+        setTemplateOptions(newTemplateOptions)
+      }
+
 
         return (
         <>
-            <Typography variant="h2" textAlign={'center'} sx={{color: 'white', fontFamily: 'Caudex', py: 15, mb:10, backgroundColor:'rgb(191, 172, 224)'}} >Hi, {user.userName}</Typography>
-            
+            <Typography variant="h2" textAlign={'center'} sx={{color: 'white', fontFamily: 'Caudex', py: 15, mb:10, backgroundColor:'rgb(191, 172, 224)'}} >Hi, Saif</Typography>
               <>
                 <div style = {{justifyContent: "center", alignItems: "center", textAlign: "center"}}>
                   <TextField id="outlined-basic" 
@@ -94,14 +101,12 @@ export const HomeDashboard = (props) => {
             
             <div>
             <WeatherDashboard zipCode = {zipcode} weather = { weather }/>
-            {console.log(fetchedOutfitData)}
+              
             {zipcode !== -1 && fetchedOutfitData.hasOutfit == true &&
-              <Outfit userId = {user.userId} outfit = {outfit} fetchedOutfitData = {fetchedOutfitData} generateOutfitFunction = {fetchGeneratedOutfit} updateSinglePiece={updateSinglePiece}/>
+              <Outfit userId = {userId} outfit = {outfit} fetchedOutfitData = {fetchedOutfitData} generateOutfitFunction = {fetchGeneratedOutfit} updateSinglePiece={updateSinglePiece} onTemplateChange={onTemplateChange} templateOptions={templateOptions}/>
             }
-            {fetchedOutfitData.hasOutfit == false && fetchedOutfitData.fetchError == true &&
-              <div> 
-                <Typography variant="h5" textAlign={'center'} sx={{color: 'black', fontFamily: 'Caudex'}}> Unable to fetch an outfit at the current time. Please try again later or submit a trouble ticket. </Typography>
-              </div>
+            {fetchedOutfitData.hasOutfit == false && fetchedOutfitData.fetchError == true && 
+                <h3> Unable to fetch an outfit at the current time. Please try again later or submit a trouble ticket. </h3>
             }
             </div> 
         </>
